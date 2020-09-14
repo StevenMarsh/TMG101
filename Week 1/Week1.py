72785672ee5a93240194c5a38507c236ecfbe8f1
Steven Marsh <marshs@usc.edu>
2020-09-13 20:46:13 -0700

    Pushing the first lesson
    Basic data manipulation

--- Week 1/Week1.py
+++ Week 1/Week1.py
@@ -1,2 +1,31 @@
-import panda as pd
- kn
+import pandas as pd
+
+filename = "covid19_tweets.csv"
+
+dataframe = pd.read_csv(filename)
+
+# print(dataframe.head(5)) #print first 5 rows
+#
+# print(dataframe.tail(5)) #print last 5 rows
+#
+# print(dataframe.columns) #print name of column headers
+#
+# print(dataframe['user_followers'].describe()) #get statistics on specified headers
+
+
+
+#TODO
+
+#1. Find the average number of followers that verified users have
+
+
+
+
+
+
+#2 Look up numpy, import that library to this file. Replace all rows whose user_follower count is 0 with the average
+#number of followers (Look at image in folder to see ways of dealing with missing/bad values)
+
+
+#2. Get some cool statistic (like #1), put it in a function and then use that function to assign a value to a variable
+# and print out that variable
